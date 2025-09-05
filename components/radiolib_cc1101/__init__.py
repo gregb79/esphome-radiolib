78102b2c7f92577354f0b84637cbd38cc8e27561
gregb79 <gregbassett@bigpond.com>
2025-09-05 14:07:10 +1000

    Update __init__.py

--- components/radiolib_cc1101/__init__.py
+++ components/radiolib_cc1101/__init__.py
@@ -1,7 +1,9 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
+from esphome.components import sensor
 from esphome.components import spi
-from esphome import pins
+from esphome.automation import maybe_simple_id
+from esphome import automation, pins
 from esphome.const import *
 from esphome.cpp_helpers import gpio_pin_expression
 from esphome.core import CORE
@@ -29,11 +31,6 @@ CONFIG_SCHEMA = (
         cv.Optional(CONF_RX_PIN): pins.internal_gpio_input_pin_schema,
         cv.Optional(CONF_FREQUENCY, default="433.92MHz"): cv.frequency,
         cv.Optional(CONF_MODULATION, default="ook"): cv.enum(CC1101_MODULATIONS, upper=True, space="_"),
-        cv.Optional(CONF_FILTER, default="464kHz"): cv.frequency,
-        cv.Optional('bitrate',default=5): cv.float_range(0.025,600), # 40k = 25us resolution, 5k=less noisy
-        cv.Optional('reg_agcctrl0',default=0xb2): cv.hex_uint8_t,
-        cv.Optional('reg_agcctrl1',default=0x00): cv.hex_uint8_t,
-        cv.Optional('reg_agcctrl2',default=0xc7): cv.hex_uint8_t,
         })
     .extend(cv.COMPONENT_SCHEMA)
     .extend(spi.spi_device_schema(cs_pin_required=True))
@@ -62,11 +59,7 @@ async def to_code(config):
 
     cg.add(var.set_frequency(config[CONF_FREQUENCY]))
     cg.add(var.set_modulation(config[CONF_MODULATION]))
-    cg.add(var.set_filter(config[CONF_FILTER]))
-    cg.add(var.set_bitrate(config['bitrate']))
-    cg.add(var.set_reg_agcctrl0(config['reg_agcctrl0']))
-    cg.add(var.set_reg_agcctrl1(config['reg_agcctrl1']))
-    cg.add(var.set_reg_agcctrl2(config['reg_agcctrl2']))
+
 
     await cg.register_component(var, config)
     await spi.register_spi_device(var, config)
